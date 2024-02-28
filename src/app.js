e60b7764a5ff3a4870827ecee88812576e0f2825
David Lescano <lescanodavid07@gmail.com>
2024-02-28 20:54:22 -0300

    patron MVC

--- src/app.js
+++ src/app.js
@@ -1,1 +1,20 @@
-console.log('Prueba')
+const express = require('express');
+const app = express();
+const path = require('node:path')
+
+// Configuracion de vistas
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, '/views') );
+
+
+// Routes
+const mainRoutes = require('./routes/main.routes');
+
+app.use('/', mainRoutes);
+
+
+
+
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server up on port http://localhost:${PORT}`))
